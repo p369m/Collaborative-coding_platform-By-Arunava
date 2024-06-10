27e66f0056a918af1e822dc3aaf6304da620f89e
eat_code_sleep <141677292+i0am0arunava@users.noreply.github.com>
2024-06-10 12:17:09 +0530

    Update page.tsx

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -138,7 +138,7 @@ export default function PersistentDrawerLeft() {
   };
 
   useEffect(() => {
-    socket.current = io("https://mysocketserver-production.up.railway.app");
+    socket.current = io("https://generous-passion-production.up.railway.app");
     const checkUser = async () => {
       const res = await axios.get("/api/users/me");
 
